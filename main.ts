96407248a71f6705e26e938b654dcd64677a3f89
stemventuri <57156157+stemventuri@users.noreply.github.com>
2020-10-25 21:51:36 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+function main_radio_broadcast () {
+    radio.sendNumber(0)
+    basic.pause(1000)
+}
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.pause(2000)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    }
+    pins.digitalWritePin(DigitalPin.P0, 0)
+})
+function setup_radio () {
+    radio.setGroup(1)
+    radio.setTransmitPower(1)
+    radio.setTransmitSerialNumber(true)
+}
+setup_radio()
+basic.forever(function () {
+    main_radio_broadcast()
+})
